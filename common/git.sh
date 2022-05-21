bcb41e0d6270180c03bb6866589835d21ff552be
root <root@lab01.wirdemo.com>
2022-05-21 15:34:27 +0200

    Autogit: Sat May 21 15:34:27 CEST 2022

---
+++ common/git.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+ME=$0
+DIR=`dirname $ME`
+DIR=`dirname $DIR`
+
+if [ ! -d ${DIR}/.git ]; then
+	cd ${DIR}/.git
+	exit 1
+fi
+
+cd ${DIR} 
+
+if [[ ! `git status --porcelain` ]]; then
+	echo "Nothing changed"
+	exit 0
+fi
+
+echo "Adding changes..."
+git status
+
+echo "git add ."
+git add .
+
+echo git commit -m "Autogit: `date`"
+git commit -m "Autogit: `date`"
+
