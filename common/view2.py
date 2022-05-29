aac37c10c4a97e376657d0f3cbc001f627ea52e8
Peter Wirdemo <peter.wirdemo@gmail.com>
2022-05-29 13:50:06 +0200

    pylint refresh

--- common/view2.py
+++ common/view2.py
@@ -1,80 +1,81 @@
-from flask import Flask, Response, request
-import pprint
+#import pprint
 import os
 import socket
 import json
+from flask import Flask, Response, request
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-root = "/var/tmp/labconf"
+ROOT = "/var/tmp/labconf"
 
 
-def resolve_ip(ip):
+def resolve_ip(ip_address):
     # type: (str) -> str
     """Resolve a hostname to its IP if possible"""
-    interfaces = socket.gethostbyaddr(ip)
+    interfaces = socket.gethostbyaddr(ip_address)
     if len(interfaces) == 0:
-        return ip
+        return ip_address
     addr = interfaces[0]
-    return(addr)
+    return addr
 
-def labconf(ip):
+def labconf(ip_address):
     # type: (str) -> str
-    
+    """Try to find labonf and return it"""
+
     myjson = {}
-    res = ""
-    addr = resolve_ip(ip)
-    conf = root + "/" + addr + "/config"
+    addr = resolve_ip(ip_address)
+    conf = ROOT + "/" + addr + "/config"
     try:
-        st = os.stat(conf)
+        st_file = os.stat(conf)
     except:
-        return(mysjon)
+        return myjson
     else:
-        myjson["mtime"]=int(st.st_mtime)
+        myjson["mtime"] = int(st_file.st_mtime)
 
     try:
-       f = open(conf,"r")
+        f = open(conf, "r")
     except:
-       return(myjson)
+        return myjson
     else:
-       for x in f:
-           print("x:",x)
-           y = x.strip()
-           print("y:",y)
-           key,value = y.split(":",2)
-           if key:
-               key = key.rstrip().lstrip()
-           else:
-               continue
+        for line in f:
+            print("line:", line)
+            #y = line.strip()
+            #print("y:", y)
+            key, value = line.strip().split(":", 2)
+            if key:
+                key = key.rstrip().lstrip()
+            else:
+                continue
 
-           if value:
-               value = value.rstrip().lstrip()
-           else:
-               continue
-           myjson[key]=value
-       f.close()
+            if value:
+                value = value.rstrip().lstrip()
+            else:
+                continue
+            myjson[key] = value
+        f.close()
 
-    return(myjson)
+    return myjson
 
 
 
-@app.route('/test')
+@APP.route('/test')
 def test():
-    str = ""
+    """Function to get labconf"""
+    res = ""
     #for k, v in os.environ.items():
     #   str += k + "=" + v + "<BR>"
     #str += pprint.pformat(request.environ, depth=5) + '<BR><BR>'
     #str += pprint.pformat(request.headers, depth=5) + '<BR><BR>'
     #str += pprint.pformat(request.remote_addr, depth=5) + '<BR><BR>'
     #str += pprint.pformat(resolve_ip(request.remote_addr), depth=5) + '<BR><BR>'
-    #str += pprint.pformat(labconf(request.remote_addr), depth=5) 
+    #str += pprint.pformat(labconf(request.remote_addr), depth=5)
     myjson = labconf(request.remote_addr)
-    str = json.dumps(myjson)
+    res = json.dumps(myjson)
     #return Response(str, mimetype="text/text")
-    return Response(str)
+    return Response(res)
     #return(str)
 
-#if __name__ == "__main__":
-#    port = int(os.environ.get('PORT', 5001))
-app.run(debug=True, host='0.0.0.0', port=8443)
-
+if __name__ == "__main__":
+    PORT = 8443
+    PORT = int(os.environ.get('PORT', PORT))
+    APP.run(debug=True, host='0.0.0.0', port=PORT)
